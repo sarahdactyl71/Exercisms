cdcbe4d295ce2c51a043a01b4df5a6d9b9978a96
Sarah Kirk <kirkse710@gmail.com>
2018-09-17 11:30:13 -0700

    adds completed reverse_string.py

--- reverse-string/reverse_string.py
+++ reverse-string/reverse_string.py
@@ -1,8 +1,2 @@
 def reverse(text):
-    string = ''
-    count = -1
-    for letter in text:
-        letter = text[count]
-        string += letter
-        count += -1
-        return string
+    return text[::-1]
