5a23704dfcb43684fd87135db023db756ef701d6
Sarah Kirk <kirkse710@gmail.com>
2018-09-19 11:45:02 -0700

    reworked gigasecond after remembering that carrots aren't things in programming

--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
 
 def add_gigasecond(birth_date):
-    gigasecond = birth_date + timedelta(seconds = 10^9)
+    gigasecond = birth_date + timedelta(seconds = 10**9)
     return gigasecond
