8f755ca3a056aced8b82ffa422cfb37ff1a45679
Sarah Kirk <kirkse710@gmail.com>
2018-09-20 10:09:10 -0700

    got all tests to pass

--- bob/bob.py
+++ bob/bob.py
@@ -1,9 +1,9 @@
 def hey(phrase):
     if not phrase or phrase.isspace():
         return "Fine. Be that way!"
-    elif phrase[-1] == '?' and all_caps(phrase):
+    elif '?' in phrase and all_caps(phrase):
         return "Calm down, I know what I'm doing!"
-    elif phrase[-1] == '?':
+    elif phrase[-1] == '?' or (phrase[-1].isspace() and '?' in phrase):
         return "Sure."
     elif all_caps(phrase):
         return "Whoa, chill out!"
