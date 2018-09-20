ff815399bf665eb8e156099dff6127550c8936f7
Sarah Kirk <kirkse710@gmail.com>
2018-09-20 09:57:08 -0700

    changed order of tests, got one more passing

--- bob/bob.py
+++ bob/bob.py
@@ -1,12 +1,12 @@
 def hey(phrase):
-    if phrase[-1] == '?' and all_caps(phrase):
+    if not phrase:
+        return "Fine. Be that way!"
+    elif phrase[-1] == '?' and all_caps(phrase):
         return "Calm down, I know what I'm doing!"
     elif phrase[-1] == '?':
         return "Sure."
     elif all_caps(phrase):
         return "Whoa, chill out!"
-    elif not phrase:
-        return "Fine. Be that way!"
     else:
         return "Whatever."
 
