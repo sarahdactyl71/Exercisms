a30a2fa25a1f1ddb482ccb084d8976e95b4a6a2d
Sarah Kirk <kirkse710@gmail.com>
2018-09-21 11:45:41 -0700

    some refactoring for rounding

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -1,33 +1,34 @@
 class SpaceAge(object):
     def __init__(self, seconds):
         self.seconds = seconds
-        self.earth_seconds = 31557600
+        self.earth_years = seconds / 31557600
 
     def on_earth(self):
-        return round((self.seconds / 31557600), 2)
+        return self.makes_pretty(self.seconds)
 
     def on_mercury(self):
-        return earth_years / 0.2408467
+        return self.makes_pretty(0.2408467)
 
     def on_venus(self):
-        return earth_years / 0.61519726
+        return self.makes_pretty(0.61519726)
 
     def on_mars(self):
-        return earth_years / 1.8808158
+        return self.makes_pretty(1.8808158)
 
     def on_jupiter(self):
-        return earth_years / 11.862615
+        return self.makes_pretty(11.862615)
 
     def on_saturn(self):
-        return earth_years / 29.447498
+        return self.makes_pretty(29.447498)
 
     def on_uranus(self):
-        return earth_years / 84.016846
+        return self.makes_pretty(84.016846)
 
     def on_neptune(self):
-        return earth_years / 164.79132
+        return self.makes_pretty(164.79132)
 
-
+    def makes_pretty(self, ratio):
+        return round((self.earth_years / ratio) , 2)
 
 
 
