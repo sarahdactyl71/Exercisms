246bf302e1e583d8d6c23d930a324279b6bf143f
Sarah Kirk <kirkse710@gmail.com>
2018-09-26 14:58:16 -0700

    adds some to the power method

--- rational-numbers/rational_numbers.py
+++ rational-numbers/rational_numbers.py
@@ -47,7 +47,10 @@ class Rational(object):
         # The absolute value |r| of the rational number r = a/b is equal to |a|/|b|.
 
     def __pow__(self, power):
-        pass
+         if power < 0:
+            self.numer, self.denom  = self.denom, self.numer
+            power = -power
+            return Rational(self.numer ** power, self.denom ** power)
 
     def __rpow__(self, base):
         pass
