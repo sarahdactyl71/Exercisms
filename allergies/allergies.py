4bd307fdb42b966f53eb6be2080e0e67a2db99f1
Sarah Kirk <kirkse710@gmail.com>
2018-10-02 14:35:59 -0700

    moves allergens_dict to be a class variable

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,19 +1,20 @@
 class Allergies(object):
 
+    allergens_dict = {
+    "eggs": 1,
+    "peanuts": 2,
+    "shellfish": 4,
+    "strawberries": 8,
+    "tomatoes": 16,
+    "chocolate": 32,
+    "pollen": 64,
+    "cats": 128
+    }
+
     def __init__(self, score):
         self.score = score
 
     def is_allergic_to(self, item):
-        allergens_dict = {
-            "eggs": 1,
-            "peanuts": 2,
-            "shellfish": 4,
-            "strawberries": 8,
-            "tomatoes": 16,
-            "chocolate": 32,
-            "pollen": 64,
-            "cats": 128
-        }
         # for key, value in allergens_dict.items():
 
     @property
