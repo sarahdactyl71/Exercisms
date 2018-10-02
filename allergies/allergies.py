76513bea1ff2ce1f674e372d48976449639577a2
Sarah Kirk <kirkse710@gmail.com>
2018-10-02 14:34:52 -0700

    deletes some comments

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -4,28 +4,22 @@ class Allergies(object):
         self.score = score
 
     def is_allergic_to(self, item):
-        pass
+        allergens_dict = {
+            "eggs": 1,
+            "peanuts": 2,
+            "shellfish": 4,
+            "strawberries": 8,
+            "tomatoes": 16,
+            "chocolate": 32,
+            "pollen": 64,
+            "cats": 128
+        }
+        # for key, value in allergens_dict.items():
 
     @property
     def lst(self):
         pass
 
-# Given a person's allergy score, determine whether or not they're allergic to a
-# given item, and their full list of allergies.
-#
-# An allergy test produces a single numeric score which contains the information
-# about all the allergies the person has (that they were tested for).
-#
-# The list of items (and their value) that were tested are:
-#
-# eggs (1)
-# peanuts (2)
-# shellfish (4)
-# strawberries (8)
-# tomatoes (16)
-# chocolate (32)
-# pollen (64)
-# cats (128)
 # So if Tom is allergic to peanuts and chocolate, he gets a score of 34.
 #
 # Now, given just that score of 34, your program should be able to say:
@@ -36,13 +30,3 @@ class Allergies(object):
 # that score 256, 512, 1024, etc.). Your program should ignore those components
 # of the score. For example, if the allergy score is 257, your program should only
 # report the eggs (1) allergy.
-allergens_dict = {
-    "eggs": 1,
-    "peanuts": 2,
-    "shellfish": 4,
-    "strawberries": 8,
-    "tomatoes": 16,
-    "chocolate": 32,
-    "pollen": 64,
-    "cats": 128 
-}
